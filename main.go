d2119c25af12ddbc630e94a82623c05802b0c174
Devin Pastoor <devin.pastoor@gmail.com>
2017-02-04 11:43:40 -0500

    simple server that serves index.html by default

---
+++ main.go
@@ -0,0 +1,31 @@
+/*
+Serve is a very simple static file server in go
+Usage:
+	-p="8100": port to serve on
+	-d=".":    the directory of static files to host
+Navigating to http://localhost:8100 will display the index.html or directory
+listing file.
+*/
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"path/filepath"
+
+	"github.com/skratchdot/open-golang/open"
+)
+
+func main() {
+	port := flag.String("p", "8100", "port to serve on")
+	directory := flag.String("d", ".", "the directory of static file to host")
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir(*directory)))
+	go func() {
+		open.Run(filepath.Join(*directory, "index.html"))
+	}()
+	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
+}
