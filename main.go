fdb1c8096360a9b1de01dd287d151ea9e4276b2f
Devin Pastoor <devin.pastoor@gmail.com>
2017-06-04 13:20:43 +0200

    control whether want to launch browser

--- main.go
+++ main.go
@@ -1,35 +1,33 @@
 /*
-Serve is a very simple static file server in go
+gss is a very simple static file server in go
 Usage:
 	-p="8100": port to serve on
 	-d=".":    the directory of static files to host
+	--n      : don't launch the browser automatically
 Navigating to http://localhost:8100 will display the index.html or directory
 listing file.
 */
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"path/filepath"
 
+	flag "github.com/ogier/pflag"
+
 	"github.com/skratchdot/open-golang/open"
 )
 
 // VERSION is the version
-const VERSION = "0.0.2"
+const VERSION = "1.0.0"
 
 func main() {
 	port := flag.String("p", "8100", "port to serve on")
 	directory := flag.String("d", ".", "the directory of static file to host")
+	noLaunch := flag.Bool("n", false, "launch the served directory in a browser")
 	flag.Parse()
-
-	http.Handle("/", http.FileServer(http.Dir(*directory)))
-	go func() {
-		open.Run(fmt.Sprintf("http://localhost:%s", *port))
-	}()
 
 	dirpath, err := filepath.Abs(*directory)
 
@@ -37,7 +35,18 @@ func main() {
 		log.Fatalf("could not find directory at %s", *directory)
 	}
 
+	http.Handle("/", http.FileServer(http.Dir(dirpath)))
+	launch := !*noLaunch
+	if !*noLaunch {
+		go func() {
+			open.Run(fmt.Sprintf("http://localhost:%s", *port))
+		}()
+	}
+
 	log.Printf("gss version: %s", VERSION)
-	log.Printf("Serving %s \non HTTP port: %s\n", dirpath, *port)
+	log.Printf("Serving from directory: \n %s \n on HTTP port: %s\n", dirpath, *port)
+	if launch {
+		log.Printf("launching browser...")
+	}
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
